cdcb2a2810629c160146ec66176f76759d3d647b
James O'Cull <james.ocull@gmail.com>
2024-04-29 23:22:00 -0400

    Fix remaining quirks with YoutubeAudioSourceManager to exclude old version

--- src/main/kotlin/dev/arbjerg/ukulele/audio/LavaplayerConfig.kt
+++ src/main/kotlin/dev/arbjerg/ukulele/audio/LavaplayerConfig.kt
@@ -1,8 +1,18 @@
 package dev.arbjerg.ukulele.audio
 
+import com.sedmelluq.discord.lavaplayer.container.MediaContainerRegistry
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers
+import com.sedmelluq.discord.lavaplayer.source.bandcamp.BandcampAudioSourceManager
+import com.sedmelluq.discord.lavaplayer.source.beam.BeamAudioSourceManager
+import com.sedmelluq.discord.lavaplayer.source.getyarn.GetyarnAudioSourceManager
+import com.sedmelluq.discord.lavaplayer.source.http.HttpAudioSourceManager
+import com.sedmelluq.discord.lavaplayer.source.nico.NicoAudioSourceManager
+import com.sedmelluq.discord.lavaplayer.source.soundcloud.SoundCloudAudioSourceManager
+import com.sedmelluq.discord.lavaplayer.source.twitch.TwitchStreamAudioSourceManager
+import com.sedmelluq.discord.lavaplayer.source.vimeo.VimeoAudioSourceManager
+import com.sedmelluq.discord.lavaplayer.source.yamusic.YandexMusicAudioSourceManager
 import dev.lavalink.youtube.YoutubeAudioSourceManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -16,11 +26,17 @@ class LavaplayerConfig {
         // Add the new YoutubeAudioSourceManager
         apm.registerSourceManager(YoutubeAudioSourceManager(true))
 
-        // Then add the rest of the source managers
-        AudioSourceManagers.registerRemoteSources(apm)
-
-        // TODO: Is there a way to take out the original YoutubeAudioSourceManager?
-        // apm.sourceManagers.removeAll { audioSourceManager -> audioSourceManager is com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager }
+        // Port the rest from `com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers.registerRemoteSources`
+        // while excluding the legacy `YoutubeAudioSourceManager`
+        apm.registerSourceManager(YandexMusicAudioSourceManager(true))
+        apm.registerSourceManager(SoundCloudAudioSourceManager.createDefault())
+        apm.registerSourceManager(BandcampAudioSourceManager())
+        apm.registerSourceManager(VimeoAudioSourceManager())
+        apm.registerSourceManager(TwitchStreamAudioSourceManager())
+        apm.registerSourceManager(BeamAudioSourceManager())
+        apm.registerSourceManager(GetyarnAudioSourceManager())
+        apm.registerSourceManager(NicoAudioSourceManager())
+        apm.registerSourceManager(HttpAudioSourceManager(MediaContainerRegistry.DEFAULT_REGISTRY))
 
         return apm
     }
